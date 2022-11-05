16688d77ff6ccd69022270b18f2e27f20138f8d2
Santiago Berges <sberges@frba.utn.edu.ar>
2022-11-05 21:02:10 +0000

    CPU - SET y ADD funcionales Falta leer kernel interrupt, comenzar a pensar las direcciones y sus estructuras.

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -16,17 +16,23 @@
 t_log* logger;
 t_config* cpu_config;
 uint32_t inputs_tlb;
-//
-int instruction_delay, interruption_occurred;
-float instruction_delay_sec;
+int instruction_delay,
+	interruption_quantum,
+	interruption_io_pf,
+	socket_cpu_dispatch,
+	socket_kernel_dispatch,
+	instruction_delay_sec;
 char* replace_tlb;
 pthread_t thread_interrupt;
 //sem_t sem_interrupt;
 
 
 int main(int argc, char **argv) {
-	// Inicio logger
+
+	// Logger
+
 	logger = log_create("cpu.log", "CPU", true, LOG_LEVEL_TRACE);
+
 	// Obtengo path de Config
 
 	char* cpu_config_path = argv[1];
@@ -47,9 +53,6 @@ int main(int argc, char **argv) {
 	log_trace(logger, "  RETARDO_INSTRUCCION: %i ms", instruction_delay);
 	instruction_delay_sec = instruction_delay * 0.001; // para poder utilizar la variable dentro de sleep
 
-
-
-
 	// Error por si no se paso bien el argumento
 
 	if(cpu_config == NULL) {
@@ -57,87 +60,11 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-	// CONECTAR A MEMORIA
+	connections();
 
-	// CONECTAR A KERNEL
+	instruction_cycle();
 
-	pthread_create(&thread_interrupt, NULL, start_interrupt, NULL); // thread interrupt
-
-	// DISPATCH
-
-	int socket_cpu_dispatch = start_server_module("CPU_DISPATCH");
-	log_trace(logger,"Esperando conexion con Kernel desde DISPATCH");
-	int socket_kernel_dispatch = accept(socket_cpu_dispatch, NULL, NULL);
-
-	// QUEDA BLOQUEADO HASTA TENER CONEXIÓN CON KERNEL_DISPATCH
-
-	log_trace(logger, "Conexion con kernel dispatch: %i", socket_kernel_dispatch);
-	t_pcb* pcb = recv_pcb(socket_kernel_dispatch);
-	/*pcb->interrupt_type = NO_INTERRUPT;
-	while(pcb->interrupt_type == NO_INTERRUPT){
-		// FETCH
-
-		t_instruction* instruction_fetched = list_get(pcb->instructions,pcb->program_counter);
-
-		// DECODE
-
-		switch(instruction_fetched->instruction){
-			case SET:
-
-				t_register* reg1 = list_get(instruction_fetched->parameters,0);
-				uint32_t param1 = list_get(instruction_fetched->parameters,1);
-				set_execute();
-
-				break;
-			case ADD:
-
-				t_register* reg1 = list_get(instruction_fetched->parameters,0);
-				t_register* reg2 = list_get(instruction_fetched->parameters,1);
-
-				break;
-			case MOV_IN:
-
-				t_register* reg1 = list_get(instruction_fetched->parameters,0);
-				uint32_t param1 = list_get(instruction_fetched->parameters,1);
-
-				break;
-			case MOV_OUT:
-
-				uint32_t param1 = list_get(instruction_fetched->parameters,0);
-				t_register* reg1 = list_get(instruction_fetched->parameters,1);
-
-				break;
-			case IO:
-
-
-
-				break;
-			case EXIT:
-				// Guardar directo en pcb para hacer check al final
-				interruption_occurred = EXECUTION_FINISHED;
-
-				break;
-			}
-
-		pcb->program_counter++;
-
-		// CHECK INTERRUPT
-		if (interruption_occurred != NO_INTERRUPT){
-			pcb->interrupt_type = interruption_occurred;
-		}
-		interruption_occurred = NO_INTERRUPT;
-	}*/
-	pcb->program_counter++;
-	log_pcb(logger, pcb);
-	send_pcb(socket_kernel_dispatch, pcb);
-	//sem_post(sem_interrupt);
-
-	log_trace(logger, "Envio PCB modificado");
-
-	//FREE MEM
-
-	config_destroy(cpu_config);
-	log_destroy(logger);
+	free_memory();
 
 	// EXIT
 	return EXIT_SUCCESS;
@@ -147,20 +74,165 @@ int main(int argc, char **argv) {
 
 void* start_interrupt(void* arg) {
 	// FUNCION PARA THREAD
+
 	int socket_cpu_interrupt = start_server_module("CPU_INTERRUPT");
 	log_trace(logger,"Esperando conexion con Kernel desde INTERRUPT");
 	int socket_kernel_interrupt = accept(socket_cpu_interrupt, NULL, NULL);
 	log_trace(logger, "Conexion con kernel interrupt: %i", socket_kernel_interrupt);
+
 	/*while(true){
 		// Introducir recibir interrupcion   EXECUTION_FINISHED > INT_IO = PAGE_FAULT > INT_QUANTUM FIJARSE CONDICIONES CARRERA
-		interruption_occured = INT_QUANTUM;
+		interruption_quantum = INT_QUANTUM;
 		sem_wait(sem_interrupt);
-		interruption_occured = NO_INTERRUPT;
+		interruption_quantum = NO_INTERRUPT;
 	}*/
 }
-/*
+
+
+
+
+
+void connections(){
+
+	// CONECTAR A MEMORIA
+
+	// CONECTAR A KERNEL
+
+	// INTERRUPT
+
+	pthread_create(&thread_interrupt, NULL, start_interrupt, NULL); // thread interrupt
+
+	// DISPATCH
+
+	socket_cpu_dispatch = start_server_module("CPU_DISPATCH");
+	log_trace(logger,"Esperando conexion con Kernel desde DISPATCH");
+	socket_kernel_dispatch = accept(socket_cpu_dispatch, NULL, NULL);
+
+	// QUEDA BLOQUEADO HASTA TENER CONEXIÓN CON KERNEL_DISPATCH
+
+	log_trace(logger, "Conexion con kernel dispatch: %i", socket_kernel_dispatch);
+
+}
+
+
+
+void instruction_cycle(){
+
+	while(true){
+		t_pcb* pcb = recv_pcb(socket_kernel_dispatch);
+		pcb->interrupt_type = NO_INTERRUPT;
+		interruption_quantum = NO_INTERRUPT;
+		interruption_io_pf = NO_INTERRUPT;
+		log_trace(logger, "Recibi PCB y ciclo instruccion");
+		while(pcb->interrupt_type == NO_INTERRUPT){
+			// FETCH
+
+			t_instruction* instruction_fetched = list_get(pcb->instructions,pcb->program_counter);
+
+			//log_instructions(logger, pcb->instructions);
+
+			log_trace(logger, "FETCH COMPLETO");
+			// DECODE & EXECUTE
+
+			t_parameter* param1;
+			t_parameter* param2;
+
+			if(instruction_fetched->instruction != EXIT){
+
+				param1 = (t_parameter*)list_get(instruction_fetched->parameters,0);
+				param2 = (t_parameter*)list_get(instruction_fetched->parameters,1);
+
+			}
+			switch(instruction_fetched->instruction){
+				case SET:
+
+					set_execute(pcb, (t_register)param1->parameter, (uint32_t)param2->parameter);
+
+					break;
+				case ADD:
+
+					add_execute(pcb, (t_register)param1->parameter, (t_register)param2->parameter);
+
+					break;
+				case MOV_IN:
+
+					//t_register reg1 = list_get(instruction_fetched->parameters,0);
+					//uint32_t param1 = list_get(instruction_fetched->parameters,1);
+					//mov_in_execute(pcb, reg1, param1);
+
+					break;
+				case MOV_OUT:
+
+					//uint32_t param1 = list_get(instruction_fetched->parameters,0);
+					//t_register reg1 = list_get(instruction_fetched->parameters,1);
+
+					break;
+				case IO:
+
+
+
+					break;
+				case EXIT:
+					// Guardar directo en pcb para hacer check al final
+					pcb->interrupt_type = EXECUTION_FINISHED;
+
+					break;
+			}
+			log_trace(logger, "DECODE/EXECUTE COMPLETO");
+			pcb->program_counter++;
+
+			// CHECK INTERRUPT
+
+			if(pcb->interrupt_type != EXECUTION_FINISHED){
+				if(interruption_io_pf == NO_INTERRUPT){
+					pcb->interrupt_type = interruption_quantum;
+				}
+				else{
+					pcb->interrupt_type = interruption_io_pf;
+				}
+			}
+			log_trace(logger, "CHECK INTERRUPT");
+			//log_trace(logger, "AX: %i", pcb->registers[AX]);
+			//log_trace(logger, "BX: %i", pcb->registers[BX]);
+			//log_trace(logger, "CX: %i", pcb->registers[CX]);
+			//log_trace(logger, "DX: %i", pcb->registers[DX]);
+		}
+		log_pcb(logger, pcb);
+		send_pcb(socket_kernel_dispatch, pcb);
+		//sem_post(sem_interrupt);
+		log_trace(logger, "Envio PCB modificado");
+
+	}
+}
+
+
+
+void free_memory(){
+
+	config_destroy(cpu_config);
+	log_destroy(logger);
+
+}
+
+
+
 void set_execute(t_pcb* pcb, t_register reg1, uint32_t param1){
+
 	pcb->registers[reg1] = param1;
 	sleep(instruction_delay_sec);
+
 }
-*/
+
+void add_execute(t_pcb* pcb, t_register reg1, t_register reg2){
+
+	pcb->registers[reg1] = pcb->registers[reg1]+pcb->registers[reg2];
+	sleep(instruction_delay_sec);
+
+}
+
+/*void mov_in_execute(t_pcb* pcb, t_register reg1, uint32_t param1){
+
+
+
+
+}*/
--- cpu/src/cpu.h
+++ cpu/src/cpu.h
@@ -1,3 +1,17 @@
 
 
 void* start_interrupt(void* arg);
+
+//void setup();
+
+void connections();
+
+void instruction_cycle();
+
+void free_memory();
+
+void set_execute(t_pcb* pcb, t_register reg1, uint32_t param1);
+
+void add_execute(t_pcb* pcb, t_register reg1, t_register reg2);
+
+// void mov_in_execute(t_pcb* pcb, t_register reg1, uint32_t param1);
